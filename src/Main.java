07ee036c191aedd45d65ab39eda7356fa081033d
Grigoriy <grigoriy.nenkov@mail.ru>
2026-03-11 18:05:54 +0300

    second commit

--- src/Main.java
+++ src/Main.java
@@ -11,31 +11,23 @@
 
 public class Main {
     public static void main(String[] args) {
-        test1(6100);
         test1(6750);
-        test1(350);
     }
 
-    public static int test1(int count) {
+    public static void test1(int count) {
         int[] array = {5000, 1000, 500, 100, 50};
+        int k;
 
         if (count % 50 == 0) {
             for (int i = 0; i < array.length; i++) {
-                if (count - array[i] > 0) {
-                    count -= array[i];
-                    if (count < 450) {
-                        while (count >= 100) {
-                            count -= array[3];
-                            if (count == 50) {
-                                count -= array[4];
-                            }
-                        }
-                    }
+                if (count >= array[i]) {
+                    k = count / array[i];
+                    count = count - k * array[i];
+                    System.out.println(k + " шт по " + array[i]);
                 }
             }
         } else {
             System.out.println("error");
         }
-        return count;
     }
 }
